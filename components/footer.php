b964b3e29f9f0fc9741aed19ff1078f86063d613
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-07-14 20:32:40 +0530

    minor change

--- components/footer.php
+++ components/footer.php
@@ -20,10 +20,10 @@
 
       <div class="box">
          <h3>contact us</h3>
-         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123 456 7899</a>
-         <a href="tel:11122233333"><i class="fas fa-phone"></i> +111 222 3333</a>
-         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> shaikh@gmail.com</a>
-         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </a>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +91 63xxxxxx95</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +91 75xxxxxx00</a>
+         <a href="mailto:iamabhaytiwari343@gmail.com"><i class="fas fa-envelope"></i> iamabhaytiwari343@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Lucknow, india - 226016 </a>
       </div>
 
       <div class="box">
@@ -36,6 +36,6 @@
 
    </section>
 
-   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+   <div class="credit"><?= date('Y'); ?> | by <a href="#"><span>Abhay Tiwari</span></a> | all rights reserved!</div>
 
 </footer>
