aa28d3a4168e84bb84e2bca8e04f4c654c6f7ce2
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-07-14 22:57:31 +0530

    update

--- admin/admin_accounts.php
+++ admin/admin_accounts.php
@@ -1,23 +1,17 @@
 <?php
 include '../components/connect.php';
-
 session_start();
-
 $admin_id = $_SESSION['admin_id'];
-
 if(!isset($admin_id)){
    header('location:admin_login.php');
 }
-
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
    $delete_admins = $conn->prepare("DELETE FROM `admins` WHERE id = ?");
    $delete_admins->execute([$delete_id]);
    header('location:admin_accounts.php');
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,27 +19,18 @@ if(isset($_GET['delete'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>admin accounts</title>
-
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
    <link rel="stylesheet" href="../css/admin_style.css">
-
 </head>
 <body>
-
 <?php include '../components/admin_header.php'; ?>
-
 <section class="accounts">
-
    <h1 class="heading">admin accounts</h1>
-
    <div class="box-container">
-
    <div class="box">
       <p>add new admin</p>
       <a href="register_admin.php" class="option-btn">register admin</a>
    </div>
-
    <?php
       $select_accounts = $conn->prepare("SELECT * FROM `admins`");
       $select_accounts->execute();
@@ -70,9 +55,7 @@ if(isset($_GET['delete'])){
          echo '<p class="empty">no accounts available!</p>';
       }
    ?>
-
    </div>
-
 </section>
 
 
--- admin/admin_login.php
+++ admin/admin_login.php
@@ -1,16 +1,11 @@
 <?php
-
 include '../components/connect.php';
-
 session_start();
-
 if(isset($_POST['submit'])){
-
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
    $pass = sha1($_POST['pass']);
    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
-
    $select_admin = $conn->prepare("SELECT * FROM `admins` WHERE name = ? AND password = ?");
    $select_admin->execute([$name, $pass]);
    $row = $select_admin->fetch(PDO::FETCH_ASSOC);
@@ -21,11 +16,8 @@ if(isset($_POST['submit'])){
    }else{
       $message[] = 'incorrect username or password!';
    }
-
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,14 +25,10 @@ if(isset($_POST['submit'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>login</title>
-
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
    <link rel="stylesheet" href="../css/admin_style.css">
-
 </head>
 <body>
-
 <?php
    if(isset($message)){
       foreach($message as $message){
@@ -53,9 +41,7 @@ if(isset($_POST['submit'])){
       }
    }
 ?>
-
 <section class="form-container">
-
    <form action="" method="post">
       <h3>login now</h3>
       <p>default username = <span>admin</span> & password = <span>111</span></p>
@@ -63,8 +49,6 @@ if(isset($_POST['submit'])){
       <input type="password" name="pass" required placeholder="enter your password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
       <input type="submit" value="login now" class="btn" name="submit">
    </form>
-
-</section>
-   
+</section>  
 </body>
 </html>
